6fcd5aa91603b5c0ebc4f902dbe7f12fb64bc644
sarah wang <sarahwang47@gmail.com>
2020-10-02 16:57:34 +0800

    add destroy status

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   post 'sessions/create' => 'sessions#create'
-  get 'sessions/delete'
+  delete 'sessions/destroy' => 'sessions#destroy'
   root 'page#home'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
