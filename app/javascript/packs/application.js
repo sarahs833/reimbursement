86a2ead39b302bbd0025856de53e401172df656c
sarah wang <sarahwang47@gmail.com>
2020-10-03 15:47:41 +0800

    timepicker english

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,17 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("moment/locale/ja")
 require("tempusdominus-bootstrap-4")
 import '../stylesheets/application'
 import jquery from 'jquery';
 window.$ = window.jquery = jquery;
 
-$('.datetimepicker').datetimepicker();
+$(function() {
+  $('#datetimepicker4').datetimepicker({
+    format: 'L'
+  });
+});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
