6f704b4dd541079ec580e7eadcf59d44768200ac
sarah wang <sarahwang47@gmail.com>
2020-10-19 21:35:16 +0800

    add redis url env file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0', size: 1, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], size: 1, network_timeout: 5 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0', size: 12, network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], size: 12, network_timeout: 5 }
 end
